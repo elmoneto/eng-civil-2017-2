cb99867ef8e88bf20121e9a171e346cd3efc9151
Elmo Neto <eneto94@gmail.com>
2017-10-19 20:51:03 -0200

    Correção em nome de varíavel da lista 3 ex 8

--- lista3/ex8.py
+++ lista3/ex8.py
@@ -1,12 +1,12 @@
 #Exercício 8
 #Ler um número e escrever se ele "é primo" ou "não é primo". Obs.: Números primos são divisíveis somente por 1 e por ele mesmo.
 
-multiplos = 0
+divisores = 0
 number = int(input("Informe o numero: "))
 for i in range(1,number+1):
    if(number % i == 0):
-      multiplos += 1
-if(multiplos <= 2):
+      divisores += 1
+if(divisores <= 2):
    print("O numero é primo")
 else:
    print("O numero não é primo")
