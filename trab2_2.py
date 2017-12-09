583746e33d118d172457a2b92c79c96cf2561c32
Elmo Neto <eneto94@gmail.com>
2017-12-09 10:44:55 -0200

    :octocat: Adição de comentários explicativos

--- trab2_2.py
+++ trab2_2.py
@@ -4,19 +4,22 @@
 #3. Informar nome do aluno e sua nota em cada uma das avaliações
 #4. Imprimir nome, média e status (aprov/reprov) de cada aluno
 
+listaPesos = [] #lista que armazena o peso de cada uma das avaliações
+somaPesos = 0 #somatório dos pesos de todas as avaliações
+listaNomes = [] #lista que armazena nomes de todos os alunos
+listaMedias = [] #lista que armazena médias de todos os alunos
+
+
+#DEFINICAO DE QUANTIDADE DE AVALIAÇÕES E PESO DE CADA UMA DELAS
 notaMinima = float(input("Informe a nota mínima para aprovação: "))
 qtdeAvaliacoes = int(input("Informe a quantidade de avaliações: "))
-listaPesos = []
-somaPesos = 0
-
-#DEFINICAO DE NUMERO DE ALUNOS; E DE QUANTIDADE E PESO DE AVALIAÇÕES
 for i in range(0,qtdeAvaliacoes):
    peso = float(input("Informe o peso da " + str(i+1) + "a avaliacao: "))
    listaPesos.append(peso)
    somaPesos = somaPesos + peso
+   
+#DEFINIÇÃO DA QUANTIDADE DE ALUNOS
 qtdeAlunos = int(input("Informe a quantidade de alunos: "))
-listaNomes = []
-listaMedias = []
 
 #ENTRADA DE NOMES E NOTAS DE ALUNOS
 for i in range(0,qtdeAlunos):
